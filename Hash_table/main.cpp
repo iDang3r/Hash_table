2ea2a10efef6a1afe98872385c34ee22ea043e44
Alex <Rusin.AI@phystech.edu>
2020-04-28 08:03:05 +0300

    start

--- Hash_table/main.cpp
+++ Hash_table/main.cpp
@@ -1,15 +1,10 @@
-//
-//  main.cpp
-//  Hash_table
-//
-//  Created by Александр on 28.04.2020.
-//  Copyright © 2020 Alex. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char* argv[]) {
+    
+    
+    
+    
+    
     return 0;
 }
