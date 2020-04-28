21d8d93a99596dd46c93eca1f8cb90c6e0528f4a
Alex <Rusin.AI@phystech.edu>
2020-04-28 08:01:33 +0300

    Initial Commit

---
+++ Hash_table/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hash_table
+//
+//  Created by Александр on 28.04.2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
